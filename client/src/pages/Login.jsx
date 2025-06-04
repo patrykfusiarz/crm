6c20967173cb0238c84477222a8a9e9c4ab87b90
Your Name <your.email@example.com>
2025-06-04 09:43:58 -0400

    Fix border radius consistency between dev and production

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -84,7 +84,7 @@ export default function Login() {
                    className="absolute inset-y-0 right-0 w-10 flex items-center group"
                    onClick={() => setShowPassword(!showPassword)}
                  >
-                   <div className="w-full h-full flex items-center justify-center rounded-r border-l border-t border-r border-b border-gray-200 hover:bg-white transition-colors cursor-pointer"><svg className="h-4 w-4 text-gray-500 group-hover:text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                   <div className="w-full h-full flex items-center justify-center rounded-r-md border-l border-t border-r border-b border-gray-200 hover:bg-white transition-colors cursor-pointer"><svg className="h-4 w-4 text-gray-500 group-hover:text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                      {showPassword ? (
                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 11-4.243-4.243m4.242 4.242L9.88 9.88" />
                      ) : (
